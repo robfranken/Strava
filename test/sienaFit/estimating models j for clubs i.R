df1204a03565eae0073ecfb4101d8434439ad90c
JochemTolsma <j.tolsma@ru.nl>
2021-12-04 10:33:09 +0100

    bunch of models

--- test/sienaFit/estimating models j for clubs i.R
+++ test/sienaFit/estimating models j for clubs i.R
@@ -12,13 +12,14 @@ c=5 #clubs
 m=6 #models (indeg, avAlt, avAttHigher, avAttLower, avAttHigher+Lower, avSim)
 
 # set the algorithm; perhaps tweak a bit?
-myalgorithm <- sienaAlgorithmCreate(projname = "test")
+myalgorithm <- sienaAlgorithmCreate(projname = "test", nsub=5, n3=5000 )
 
+# siena07( myalgorithm, data = mydata, effects = myeff[[j]], prevAns= sienaFit[[j]], returnDeps=TRUE, useCluster=TRUE, nbrNodes=10, initC=TRUE, batch=TRUE)
 
 #####
 
-
-for (i in 1:c) { # for every club
+# i in 1:c
+for (i in 2:c) { # for every club
 
   # we load the RSiena object
   load(file=paste("test", "/", "mydata", "/", "mydata_club", i, ".RData", sep = "")) 
@@ -28,21 +29,26 @@ for (i in 1:c) { # for every club
   
   # we make a list for storing the RSiena fit objects
   sienaFit <- list()
-  
+
   # for club i we run models j in 1:m
   for (j in 1:m) {
  
     # we estimate the model
     try <- 1
     print(paste("Estimating model ", j, " for club ", i, sep=""))
-    sienaFit[[j]] <- siena07(myalgorithm, data = mydata, effects = myeff[[j]], returnDeps=TRUE) # store it in the list
+    sienaFit[[j]] <- siena07(myalgorithm, data = mydata, effects = myeff[[j]], returnDeps=TRUE,
+                             useCluster=TRUE, nbrNodes=10, initC=TRUE, batch=TRUE) # store it in the list
     
-    # re-run until we reach adequate convergence
+    # re-run until we reach adequate convergence (DONT WE NEED A BREAK!?)
     while (TRUE){
       if(sienaFit[[j]]$tconv.max >= .25){
         try <- try + 1
+        if (try>30) {
+          print(paste("Now it lasted to long!") 
+          break      
+        }
         print(paste("Model did not converge adequately (", sienaFit[[j]]$tconv.max, "); ", "Repeat the estimation (", "try ", try, ")", sep = ""))
-        sienaFit[[j]] <- siena07( myalgorithm, data = mydata, effects = myeff[[j]], prevAns= sienaFit[[j]], returnDeps=TRUE)
+        sienaFit[[j]] <- siena07( myalgorithm, data = mydata, effects = myeff[[j]], prevAns= sienaFit[[j]], returnDeps=TRUE, useCluster=TRUE, nbrNodes=10, initC=TRUE, batch=TRUE)
       }else{
         print(paste("Reached overall maximum convergence ratio of: ", sienaFit[[j]]$tconv.max, sep = ""))
         print("")
@@ -59,4 +65,17 @@ for (i in 1:c) { # for every club
   
 }
 
+#your objects have the same name, thus I guess you will need something like this. 
+sienaFit_clubL <- list()
+
+for (i in 1:5) {
+  temp.space <- new.env()
+  bar <- load(paste("test/sienaFit/sienaFit_club", i, ".RData", sep=""), temp.space)
+  sienaFit_clubL[[i]] <- get(bar, temp.space)
+  rm(temp.space)
+}
+
+
+lapply(sienaFit_clubL, '[[', 5)
+map(sienaFit_clubL, 6)
 
